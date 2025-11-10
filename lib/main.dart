671f3cb776463b8c7a2acd10f46f4d210db133fc
WidintonyT <wytaimbud22a@gmail.com>
2025-11-10 13:40:59 -0500

    Actualizacion de iconos del aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Onboarding Screen',
+      title: 'ReCrea',
       home: OnboardingScreen(),
       debugShowCheckedModeBanner: false,
     );
